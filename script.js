02e90e32b6c9afa5aca64398a2ed2a5b9ad78cbf
Ryan Worthey <ryan.worthey@live.com>
2024-07-16 15:03:50 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    var navMenu = document.getElementById('nav-menu');
+    navMenu.classList.toggle('active');
+});
